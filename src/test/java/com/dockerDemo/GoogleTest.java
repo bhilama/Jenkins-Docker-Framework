1d46741d64b3455f435d70465cca6f8745a3d601
bhilama <infy.maheshb@gmail.com>
2025-11-22 19:39:23 -0500

    Added test tag to test method

--- src/test/java/com/dockerDemo/GoogleTest.java
+++ src/test/java/com/dockerDemo/GoogleTest.java
@@ -1,7 +1,10 @@
 package com.dockerDemo;
 
+import org.testng.annotations.Test;
+
 public class GoogleTest extends BaseTest{
 
+    @Test
     public void landingPageTest() throws InterruptedException{
         driver.get("https://www.orangehrm.com/en");
         System.out.println("Page Title: " + driver.getTitle());
